ba1ec64de22cee135006895d767cf4af779f5bb6
Luis Angel Rodriguez <luisrodj1990@gmail.com>
2025-03-18 08:00:31 +0100

    Modificacion Se agrega proyecto api rest con get players

---
+++ API.Bet/Controllers/JugadorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using API.Bet.Data;
+using API.Bet.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Bet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JugadorController : ControllerBase
+    {
+        private readonly ILogger<JugadorController> _logger;
+        private readonly DataContext _context;
+        public JugadorController(DataContext _context, ILogger<JugadorController> logger) 
+        {
+            _logger = logger;
+            this._context = _context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<VMPlayer>>> Get()
+        {
+            List<VMPlayer> lstPlayers = await _context.C_Player.ToListAsync();
+            return Ok(lstPlayers);
+        }
+
+
+
+
+    }
+}
---
+++ API.Bet/ViewModel/VMPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Bet.ViewModel
+{
+    public class VMPlayer
+    {
+        [Key]
+        public int IDIn { get; set; }
+        public string? PlayerNameVc{ get; set; }
+        public DateTime BirthDateDt { get; set; }
+        public string? NationalityVc { get; set; }
+        public string? PicturePathVc { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
